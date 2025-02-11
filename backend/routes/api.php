c54e2a8d1076823e7672817c645101f3952868a5
Joaov535 <joaovictorcruz535@gmail.com>
2025-02-11 18:32:51 -0300

    add: nome das rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,15 +4,15 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-Route::middleware('auth:sanctum')->get('/tasks', [TaskController::class, 'load']);
-Route::middleware('auth:sanctum')->post('/tasks', [TaskController::class, 'store']);
-Route::middleware('auth:sanctum')->put('/tasks/{id}', [TaskController::class, 'update']);
-Route::middleware('auth:sanctum')->delete('/tasks/{id}', [TaskController::class, 'delete']);
+Route::middleware('auth:sanctum')->get('/tasks', [TaskController::class, 'load'])->name('load');
+Route::middleware('auth:sanctum')->post('/tasks', [TaskController::class, 'store'])->name('store');
+Route::middleware('auth:sanctum')->put('/tasks/{id}', [TaskController::class, 'update'])->name('update');
+Route::middleware('auth:sanctum')->delete('/tasks/{id}', [TaskController::class, 'delete'])->name('delete');
 
 Route::fallback(function () {
     return response()->json([

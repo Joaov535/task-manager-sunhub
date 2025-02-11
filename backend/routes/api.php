589e5f7f296daf1c76f9d9964ed16c6607aa83de
Joaov535 <joaovictorcruz535@gmail.com>
2025-02-11 18:24:40 -0300

    add: rotas da API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::middleware('auth:sanctum')->post('/tasks', [TaskController::class, 'store']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
 
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->get('/tasks', [TaskController::class, 'load']);
+Route::middleware('auth:sanctum')->post('/tasks', [TaskController::class, 'store']);
+Route::middleware('auth:sanctum')->put('/tasks/{id}', [TaskController::class, 'update']);
+Route::middleware('auth:sanctum')->delete('/tasks/{id}', [TaskController::class, 'delete']);
+
+Route::fallback(function () {
+    return response()->json([
+        'error' => 'Rota não encontrada',
+        'message' => 'A rota que você tentou acessar não existe.'
+    ], 404);
+});

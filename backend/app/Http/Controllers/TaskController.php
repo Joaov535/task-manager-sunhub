49e520af75ff2c41251cdb3e4effccb3c641ce85
Joaov535 <joaovictorcruz535@gmail.com>
2025-02-10 21:21:01 -0300

    add: tasks controller

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'status' => 'required|string|in:pendente,em andamento,concluída',
+        ]);
+
+        $user = $request->user();
+
+        $task = Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'status' => $request->status,
+            'user_id' => $user->id
+        ]);
+
+        return response()->json($task, 201);
+    }
+}

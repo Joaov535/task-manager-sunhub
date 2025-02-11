598ae518283fc506d67b15073a8453081af3c8e6
Joaov535 <joaovictorcruz535@gmail.com>
2025-02-11 18:21:49 -0300

    add: entidade Task

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'user_id'
+    ];
+
+    public static function createTask($data, $userId)
+    {
+        return self::create([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'status' => $data['status'],
+            'user_id' => $userId
+        ]);
+    }
+
+    public static function getAllTasks($userID)
+    {
+        return self::where('user_id', '=', $userID)->get();
+    }
 }
